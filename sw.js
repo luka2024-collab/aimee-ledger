d3b584430843eeaabff9efb9aba92fdd482b676e
luka2024-collab <mleluka2024@gmail.com>
2026-05-18 18:20:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,46 +1,20 @@
-const CACHE_NAME = "aimee-ledger-pwa-v14";
-
-const APP_FILES = [
-  "/aimee-ledger/",
-  "/aimee-ledger/index.html",
-  "/aimee-ledger/styles.css",
-  "/aimee-ledger/app.js",
-  "/aimee-ledger/aimee-gu.jpg",
-  "/aimee-ledger/wang.jpeg.jpeg",
-  "/aimee-ledger/reference-bg.png",
-  "/aimee-ledger/manifest.webmanifest"
-];
+const CACHE_NAME = "aimee-ledger-pwa-reset-v15";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
-  );
-  self.skipWaiting();
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  if (event.request.method === "GET") {
+    event.respondWith(fetch(event.request));
+  }
 });
